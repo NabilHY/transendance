027c65669dcae405321581f42aa1c6c3b9ea8da5
NabilHY <nabil.hayun@gmail.com>
2025-11-20 19:53:40 +0100

    Protect credentials

--- monitoring/alertmanager/entrypoint.sh
+++ monitoring/alertmanager/entrypoint.sh
@@ -8,6 +8,7 @@ cp /etc/alertmanager/alertmanager.yml.template /etc/alertmanager/alertmanager.ym
 # Alertmanager doesn't support env var substitution
 if [ -n "$SENDGRID_API_KEY" ]; then
     sed -i "s|\${SENDGRID_API_KEY}|${SENDGRID_API_KEY}|g" /etc/alertmanager/alertmanager.yml
+    sed -i "s|\${SENDGRID_EMAIL_FROM}|${SENDGRID_EMAIL_FROM}|g" /etc/alertmanager/alertmanager.yml
 fi
 
 # Start Alertmanager
