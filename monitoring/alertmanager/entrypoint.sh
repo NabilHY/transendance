5ffdfd701f4ff786b49129efcd621bae153e42bd
NabilHY <nabil.hayun@gmail.com>
2025-11-20 18:34:02 +0100

    Add entrypoint to elrtmanager remocing hardcoded env

---
+++ monitoring/alertmanager/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Copy template to writable location
+cp /etc/alertmanager/alertmanager.yml.template /etc/alertmanager/alertmanager.yml
+
+# Replace ${SENDGRID_API_KEY} placeholder with actual environment variable
+# Alertmanager doesn't support env var substitution
+if [ -n "$SENDGRID_API_KEY" ]; then
+    sed -i "s|\${SENDGRID_API_KEY}|${SENDGRID_API_KEY}|g" /etc/alertmanager/alertmanager.yml
+fi
+
+# Start Alertmanager
+exec /bin/alertmanager \
+    --config.file=/etc/alertmanager/alertmanager.yml \
+    --storage.path=/alertmanager \
+    "$@"
