f12387dc7a500e0c8768d217192042e9940fa71e
NabilHY <nabil.hayun@gmail.com>
2025-12-03 21:24:12 +0100

    Implment the delete route

--- auth-backend/src/server.js
+++ auth-backend/src/server.js
@@ -67,7 +67,7 @@ fastify.register(require('../plugins/swagger'));
 fastify.register(require('../plugins/csrf'));
 fastify.register(require('../plugins/account-lockout'));
 fastify.register(require('../plugins/account-security'));
-
+fastidy.register(require('../plugins/delete'))
 
 fastify.register(require('../routes/auth'), { prefix: '/api/auth' });
 
@@ -82,6 +82,8 @@ fastify.register(require('../routes/password-reset'), { prefix: '/api/auth' });
 fastify.register(require('../routes/email-reset'), { prefix: '/api/auth' });
 
 fastify.register(require('../routes/clear'), { prefix: '/api/auth' });
+
+fastify.register(require('../routes/delete' ), { prefix: '/api/auth' })
 
 fastify.get('/health', async (_req, reply) => {
     return reply.send({ status: 'ok' });
