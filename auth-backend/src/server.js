9969a7779aaeb1f3cd5ccbb826011e4c5d402fcf
NabilHY <nabil.hayun@gmail.com>
2025-12-04 10:59:14 +0100

    Fix healthchecks issues and typos

--- auth-backend/src/server.js
+++ auth-backend/src/server.js
@@ -100,7 +100,7 @@ fastify.register(require('../plugins/swagger'));
 fastify.register(require('../plugins/csrf'));
 fastify.register(require('../plugins/account-lockout'));
 fastify.register(require('../plugins/account-security'));
-fastidy.register(require('../plugins/delete'))
+fastify.register(require('../plugins/delete'))
 
 fastify.register(require('../routes/auth'), { prefix: '/api/auth' });
 fastify.register(require('../routes/game-token'), { prefix: '/api' });
